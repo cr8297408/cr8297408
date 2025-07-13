c39ebe221d8b10a45101911f3492b3247aa305e0
cesar <cr8297408@gmail.com>
2025-07-12 22:50:41 -0500

    fixes production styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,12 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  
+  // Configuración para GitHub Pages
+  base: '/cr8297408',
+  build: {
+    assets: '_astro'
+  },
 
   vite: {
     plugins: [tailwindcss()]
